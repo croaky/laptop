ecb7b523a5d85cbd689372389b1b40880d7c50a6
Dan Croak <dan@statusok.com>
2018-04-15 17:16:55 -0700

    install.sh: symlink tmux.conf

--- install.sh
+++ install.sh
@@ -135,6 +135,7 @@ brew cask cleanup
   ln -sf "$PWD/shell/hushlogin" "$HOME/.hushlogin"
   mkdir -p "$HOME/.ssh"
   ln -sf "$PWD/shell/ssh" "$HOME/.ssh/config"
+  ln -sf "$PWD/shell/tmux.conf" "$HOME/.tmux.conf"
   ln -sf "$PWD/shell/zshenv" "$HOME/.zshenv"
   ln -sf "$PWD/shell/zshrc" "$HOME/.zshrc"
 
