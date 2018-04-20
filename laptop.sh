237aba0ddf35be30d2a2a2698fef97224afc7517
Dan Croak <dan@statusok.com>
2018-04-20 10:47:33 -0700

    laptop.sh: upgrade to Ruby 2.5.1

--- laptop.sh
+++ laptop.sh
@@ -157,8 +157,8 @@ asdf_plugin_update() {
 
 asdf_plugin_update "ruby" "https://github.com/asdf-vm/asdf-ruby"
 asdf install ruby 2.4.2
-asdf install ruby 2.5.0
-asdf global ruby 2.5.0
+asdf install ruby 2.5.1
+asdf global ruby 2.5.1
 
 asdf_plugin_update "nodejs" "https://github.com/asdf-vm/asdf-nodejs"
 export NODEJS_CHECK_SIGNATURES=no
