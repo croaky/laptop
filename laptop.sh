753d585174047ccf7f804dd9da3727773ca406ae
Dan Croak <dan@statusok.com>
2018-04-20 09:02:15 -0700

    dotfiles: remove unused zsh config

--- laptop.sh
+++ laptop.sh
@@ -125,7 +125,6 @@ brew cask cleanup
   mkdir -p "$HOME/.ssh"
   ln -sf "$PWD/shell/ssh" "$HOME/.ssh/config"
   ln -sf "$PWD/shell/tmux.conf" "$HOME/.tmux.conf"
-  ln -sf "$PWD/shell/zshenv" "$HOME/.zshenv"
   ln -sf "$PWD/shell/zshrc" "$HOME/.zshrc"
 
   ln -sf "$PWD/sql/psqlrc" "$HOME/.psqlrc"
