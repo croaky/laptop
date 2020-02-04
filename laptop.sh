eb10a33c02c04745a3eedbe8f26ba7abdeac6bf6
Dan Croak <dan@statusok.com>
2020-02-04 11:34:21 -0800

    all: remove tmux
    I've been using Kitty tabs instead.

--- laptop.sh
+++ laptop.sh
@@ -69,10 +69,8 @@ brew "jq"
 brew "libyaml"
 brew "openssl"
 brew "protobuf"
-brew "reattach-to-user-namespace"
 brew "shellcheck"
 brew "the_silver_searcher"
-brew "tmux"
 brew "vim"
 brew "watch"
 brew "watchman"
@@ -113,16 +111,14 @@ brew cleanup
   ln -sf "$PWD/ruby/gemrc" "$HOME/.gemrc"
   ln -sf "$PWD/ruby/rspec" "$HOME/.rspec"
 
-  ln -sf "$PWD/shell/curlrc" "$HOME/.curlrc"
-  ln -sf "$PWD/shell/hushlogin" "$HOME/.hushlogin"
-
   mkdir -p "$HOME/.config/kitty"
   ln -sf "$PWD/shell/kitty.conf" "$HOME/.config/kitty/kitty.conf"
 
   mkdir -p "$HOME/.ssh"
   ln -sf "$PWD/shell/ssh" "$HOME/.ssh/config"
 
-  ln -sf "$PWD/shell/tmux.conf" "$HOME/.tmux.conf"
+  ln -sf "$PWD/shell/curlrc" "$HOME/.curlrc"
+  ln -sf "$PWD/shell/hushlogin" "$HOME/.hushlogin"
   ln -sf "$PWD/shell/zshrc" "$HOME/.zshrc"
 
   ln -sf "$PWD/sql/psqlrc" "$HOME/.psqlrc"
