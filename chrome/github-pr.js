f245e36c19439ffbdfab71c8e65cea8eda6704ec
Dan Croak <dan@statusok.com>
2020-03-29 16:49:04 -0700

    attempts at reliability, simplicity

--- chrome/github-pr.js
+++ chrome/github-pr.js
@@ -1,43 +1,29 @@
-function githubPR() {
-  function find(selector) {
-    return document.querySelectorAll(selector)[0];
+function find(selector) {
+  return document.querySelectorAll(selector)[0];
+}
+
+function sync() {
+  var commitTitle = find("input[name='commit_title']");
+  var prTitle = find("input[name='issue[title]']");
+  if (commitTitle && prTitle) {
+    commitTitle.value = prTitle.value.trim();
   }
 
-  var t;
-
-  function sync() {
-    var commitTitle = find("input[name='commit_title']");
-    var prTitle = find("input[name='issue[title]']").value.trim();
-    commitTitle.value = prTitle;
-
-    var commitMsg = find("textarea[name='commit_message']");
-    var prBody = find("textarea[name='pull_request[body]']").value.trim();
-    var prNum = find(".gh-header-number").innerHTML.trim();
-    commitMsg.innerHTML = prBody + "\n\n" + prNum;
-
-    // Forms are replaced on each XHR submit. Reset listeners on each submit.
-    clearTimeout(t);
-    t = setTimeout(function() {
-      var prTitleForm = find(".js-issue-update");
-      prTitleForm.addEventListener('submit', sync);
-      var prBodyForm = find(".js-comment-update");
-      prBodyForm.addEventListener('submit', sync);
-    }, 2500);
-  };
-
-  function isPR() {
-    var url = window.location.href
-    var parts = url.split('/').filter(function (el) { return el !== "" })
-    return parts.length === 6 && parts[4] === "pull";
+  var commitMsg = find("textarea[name='commit_message']");
+  var prBody = find("textarea[name='pull_request[body]']");
+  var prNum = find(".gh-header-number");
+  if (commitMsg && prBody && prNum) {
+    commitMsg.innerHTML = prBody.value.trim() + "\n\n" + prNum.innerHTML.trim();
   }
 
-  if (isPR()) {
-    sync();
-  };
+  // Forms are replaced on each XHR submit. Add new listeners on each submit.
+  setTimeout(function() {
+    var prTitleForm = find(".js-issue-update");
+    prTitleForm && prTitleForm.addEventListener('submit', sync);
+
+    var prBodyForm = find(".js-comment-update");
+    prBodyForm && prBodyForm.addEventListener('submit', sync);
+  }, 2000);
 };
 
-if (document.readyState != 'loading') {
-  githubPR();
-} else {
-  document.addEventListener('DOMContentLoaded', gitubPR);
-}
+sync();
