a2c51fd6862a9a427abd482a9b721a45aa45e5fc
Dan Croak <dan@statusok.com>
2020-03-29 16:57:42 -0700

    clear timeout, add code comments, sync when confirm form opens

--- chrome/github-pr.js
+++ chrome/github-pr.js
@@ -2,13 +2,17 @@ function find(selector) {
   return document.querySelectorAll(selector)[0];
 }
 
+var t;
+
 function sync() {
+  // copy PR title to commit title
   var commitTitle = find("input[name='commit_title']");
   var prTitle = find("input[name='issue[title]']");
   if (commitTitle && prTitle) {
     commitTitle.value = prTitle.value.trim();
   }
 
+  // copy PR body and number to commit message
   var commitMsg = find("textarea[name='commit_message']");
   var prBody = find("textarea[name='pull_request[body]']");
   var prNum = find(".gh-header-number");
@@ -16,13 +20,19 @@ function sync() {
     commitMsg.innerHTML = prBody.value.trim() + "\n\n" + prNum.innerHTML.trim();
   }
 
-  // Forms are replaced on each XHR submit. Add new listeners on each submit.
-  setTimeout(function() {
+  clearTimeout(t);
+  t = setTimeout(function() {
+    // listen to when PR title is updated
     var prTitleForm = find(".js-issue-update");
     prTitleForm && prTitleForm.addEventListener('submit', sync);
 
+    // listen to when PR body is updated
     var prBodyForm = find(".js-comment-update");
     prBodyForm && prBodyForm.addEventListener('submit', sync);
+
+    // listen to when squash and merge button is clicked, confirm form opens
+    var squashBtn = find(".btn-group-squash");
+    squashBtn && squashBtn.addEventListener('click', sync);
   }, 2000);
 };
 
