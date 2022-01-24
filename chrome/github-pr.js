8a887c49364a6b88ff7d9aa40420fb93cb680e17
Dan Croak <dan@statusok.com>
2022-01-24 15:13:40 -0800

    chrome/github-pr: fix CSS selector
    Also inline `el` function to avoid possible conflicts.

--- chrome/github-pr.js
+++ chrome/github-pr.js
@@ -1,20 +1,20 @@
-function find(selector) {
-  return document.querySelectorAll(selector)[0];
-}
-
 var t;
 
 function sync() {
+  var el = function (selector) {
+    return document.querySelectorAll(selector)[0];
+  };
+
   // copy PR title to commit title
-  var commitTitle = find("input[name='commit_title']");
-  var prTitle = find("input[name='issue[title]']");
+  var commitTitle = el("input[name='commit_title']");
+  var prTitle = el("input[name='issue[title]']");
   if (commitTitle && prTitle) {
     commitTitle.value = prTitle.value.trim();
   }
 
   // copy PR body and URL to commit message
-  var commitMsg = find("textarea[name='commit_message']");
-  var prBody = find("textarea[name='pull_request[body]']");
+  var commitMsg = el("textarea[name='commit_message']");
+  var prBody = el("textarea[name='issue[body]']");
   if (commitMsg && prBody) {
     commitMsg.innerHTML = [prBody.value.trim(), window.location.href]
       .filter(function (el) {
@@ -26,15 +26,15 @@ function sync() {
   clearTimeout(t);
   t = setTimeout(function () {
     // sync when PR title is updated
-    var prTitleForm = find("form.js-issue-update");
+    var prTitleForm = el("form.js-issue-update");
     prTitleForm && prTitleForm.addEventListener("submit", sync);
 
     // sync when PR body is updated
-    var prBodyForm = find("form.js-comment-update");
+    var prBodyForm = el("form.js-comment-update");
     prBodyForm && prBodyForm.addEventListener("submit", sync);
 
     // sync when squash and merge button is clicked, confirm form opens
-    var squashBtn = find("button.btn-group-squash");
+    var squashBtn = el("button.btn-group-squash");
     squashBtn && squashBtn.addEventListener("click", sync);
   }, 2000);
 }
