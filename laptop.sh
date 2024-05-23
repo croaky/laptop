a6e9bffa4cdfdc5fc0b36d91efb0f344e7035e34
Dan Croak <dan@statusok.com>
2024-05-23 13:43:04 -0400

    ruby: upgrade to 3.3.1

--- laptop.sh
+++ laptop.sh
@@ -117,7 +117,7 @@ if ! command -v godoc &> /dev/null; then
 fi
 
 # Ruby
-v="3.3.0"
+v="3.3.1"
 if [ ! -d "$HOME/.rubies/ruby-$v" ]; then
   RUBY_CONFIGURE_OPTS="--enable-yjit --with-openssl-dir=$(brew --prefix openssl@3)" ruby-build "$v" "$HOME/.rubies/ruby-$v"
 fi
